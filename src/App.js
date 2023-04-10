631b4c12136cdd8a74a34a4b20bb548fda882d95
Jayna Mukesh <115871133+JaynaMukesh@users.noreply.github.com>
2023-04-10 05:17:58 +0000

    jsssss

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import {useState} from "react";
+function App() {
+  var [count, set] = useState(0)
+  function decrement(){
+    set(count-1)
+  }
+  function increment(){
+    set(count+1)
+  }
+  function reset(){
+    set(0)
+  }
+  return (
+    <div className="">
+      <div className='flex items-center justify-center h-screen bg-[#aadaf0] flex-col text-[#323433] gap-5'>
+        <div className='font-bold text-4xl'>COUNTER APP</div>
+        <div className='text-7xl font-bold '>{count}</div>
+        <div className='flex flex-row gap-4 '>
+          <button onClick={decrement} className='bg-[#2e7d32] rounded-lg text-white px-5 py-2'>Decrement</button>
+          <button onClick={reset} className='bg-[#d32f2f] rounded-lg text-white px-5 py-2'>Reset</button>
+          <button onClick={increment} className='bg-[#1976d2] rounded-lg text-white px-5 py-2'>Increment</button>
+        </div>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
